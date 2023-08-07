0920edaca0925dc577b0ae2245a2615d0ff446f2
teddy <79746587+wofvh@users.noreply.github.com>
2023-08-07 10:21:09 +0900

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -30,7 +30,7 @@ def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbo
 
     from models.common import AutoShape, DetectMultiBackend
     from models.experimental import attempt_load
-    from models.yolo import ClassificationModel, DetectionModel
+    from models.yolo import ClassificationModel, DetectionModel, SegmentationModel
     from utils.downloads import attempt_download
     from utils.general import LOGGER, check_requirements, intersect_dicts, logging
     from utils.torch_utils import select_device
@@ -56,6 +56,7 @@ def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbo
         else:
             cfg = list((Path(__file__).parent / 'models').rglob(f'{path.stem}.yaml'))[0]  # model.yaml path
             model = DetectionModel(cfg, channels, classes)  # create model
+            model = SegmentationModel(model)
             if pretrained:
                 ckpt = torch.load(attempt_download(path), map_location=device)  # load
                 csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
